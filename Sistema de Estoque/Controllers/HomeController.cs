caadf5adc09a0a9cd2d65b1c0b24dd1faed17c40
maria <leonardojsantoos@gmail.com>
2026-07-27 09:32:07 -0300

    teste db

--- Sistema de Estoque/Controllers/HomeController.cs
+++ Sistema de Estoque/Controllers/HomeController.cs
@@ -1,32 +1,13 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Sistema_de_Estoque.Models;
 
 namespace Sistema_de_Estoque.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
+            ViewBag.DataHora = DateTime.Now;
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
